fc7feeb4f589a35692d63102bc0051ff1fc6f95d
piesfication <muhammad.rafi42@ui.ac.id>
2026-02-22 17:24:44 +0700

    Module 2 - Fix workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "3.5.0.2730"
+    id("org.sonarqube") version "7.2.2.6593"
 }
 
 group = "id.ac.ui.cs.advprog"
