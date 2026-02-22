51b24dadc5087dbddc8add5aa0d0b06e645f35fb
piesfication <muhammad.rafi42@ui.ac.id>
2026-02-22 17:45:07 +0700

    Module 2 - Trigger CI

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -30,6 +30,7 @@ public class ProductController {
     public String createProductPost(@ModelAttribute Product product, Model model) {
         service.create(product);
         return "redirect:/product/list";
+        
     }
 
     @GetMapping("/list")
